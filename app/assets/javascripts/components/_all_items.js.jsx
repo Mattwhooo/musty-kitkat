24b696deef8429cc5d1c415f1f93d6c7b484d3f6
Matt Watson <matt.watson@orasi.com>
2017-04-07 17:21:35 -0400

    initial commit

---
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -0,0 +1,22 @@
+var AllItems = React.createClass({
+    componentDidMount() {
+        $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
+    },
+    render() {
+        var items = this.state.items.map((item) => {
+            return (
+                <div key={item.id}>
+                    <h3>
+                        {item.name}
+                    </h3>
+                    <p>{item.description}</p>
+                </div>
+            )
+        });
+        return ( <div> {items} </div> )
+    },
+    getInitialState() {
+        return {items: []}
+    },
+
+});
